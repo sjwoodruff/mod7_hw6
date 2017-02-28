5ee7d0026dcc2adb1d4be706dc468d765fb51d4f
Kurt Weloth <kurtweloth@mail.weber.edu>
2017-02-28 17:23:34 +0000

    initial commit

---
+++ mod7_hw6.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  mod7_hw6.c
+ *       	Usage:  ./mod7_hw6.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/28/2017 05:22:52 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Kurt Weloth (), kurtweloth@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[ ])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
