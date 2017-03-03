ce7e403e156fbe5470e3b6b296ff0ad54f4be57d
Sterling Woodruff <sterlingwoodruff@mail.weber.edu>
2017-03-03 21:28:31 +0000

    needs work on inverse tan

--- mod7_hw6.c
+++ mod7_hw6.c
@@ -8,7 +8,7 @@
  *        Version:  1.0
  *        Created:  02/28/2017 05:22:52 PM
  *       Compiler:  gcc -Wall -Werror
- *         Author:  Kurt Weloth (), kurtweloth@mail.weber.edu
+ *         Author:  Kurt Weloth, Sterling Woodruff
  * =====================================================================================
  */
 #include <stdio.h>		/* For Standard I/O */
@@ -33,6 +33,7 @@ int main(int argc, char *argv[ ])
 {
 	int ar1,ar2;
 	double rad, ang;
+
 	if(argc != 3)
 	{
 		Usage();
@@ -40,12 +41,14 @@ int main(int argc, char *argv[ ])
 	ar1 = atof(argv[1]);
 	ar2 = atof(argv[2]);
 	// If ar1 or ar2 == 0.0 call usage again
-	if(ar1 == 0 || ar2 == 0)
+	if(ar1 == 0.0 || ar2 == 0.0)
 	{
 		Usage();
 	}
 	Polar(ar1, ar2, &rad, &ang);
 	Showit(rad,ang);
+	printf("\n");
+	AskQuestion();
 	return 0;
 }
 
@@ -57,10 +60,23 @@ void Usage(void)
 	printf("must be a floating point.\n");
 	exit(1);
 }
+
+void GetRec(double* x, double* y)
+{
+	double first, second;
+	printf("Rectangular to Polar Coordinate conversion program.\n");
+	printf("Enter the x coordinate: ");
+	scanf("%lf", &first);
+	printf("Enter the y coordinate: ");
+	scanf("%lf", &second);
+	*x = first;
+	*y = second;
+}
+
 void Polar(double x, double y, double* r, double* theta)
 {
 	*r = sqrt(pow(x, 2) + pow(y, 2));
-	*theta = atan(y/x);
+	*theta = atan (y/x) ;
 }
 void Showit(double r, double theta)
 {
@@ -68,3 +84,35 @@ void Showit(double r, double theta)
 	printf("Distance from origin: %lf\n", r);
 	printf("Angle (in degrees) from x-axis: %lf\n", theta);
 }
+
+int AskQuestion(void)
+{ 
+	int yesno;
+	double x, y;
+	double rad, ang;
+	printf("Do you want to calculate again (Y/N)?\n");
+	printf("Y = 1, N = 0: ");
+	scanf("%d", &yesno);
+	if (yesno != 1 && yesno != 0)
+	{
+		printf("Bad input, try again\n");
+		printf("\n");
+		AskQuestion();
+		return 0;
+	}
+	else if (yesno == 0)
+	{
+		printf("Done!\n");
+	}
+	else if (yesno == 1)
+	{
+		GetRec(&x, &y);
+		Polar(x, y, &rad, &ang);
+		Showit(rad, ang);
+		printf("\n");
+		AskQuestion();
+	}
+	
+	return 0;
+}
+
